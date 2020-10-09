3b4e87eb2330d6f6bf18436c60c65471942c0e48
nurtt0949 <71143068+nurtt0949@users.noreply.github.com>
2020-10-09 09:30:43 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  *
  * @author 
@@ -9,7 +10,41 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //create a scanner for user input
+    Scanner input = new Scanner(System.in);
+    //create an array for the first list of numbers
+    int [] list = new int[5];
+    //create an array for the second list of numbers
+    int [] list2 = new int[5];
+    //ask user for first list values
+    System.out.println("Please enter the values for the first array:");
+    //put integers that are in array into a for loop
+    for(int i = 0; i < 5; i++){
+      list[i] = input.nextInt();
+    }
+    //ask user for second list values
+    System.out.println("Please enter the values for the second array:");
+    //put integers that are in array into a for loop
+    for(int i = 0; i < 5; i++){
+      list2[i] = input.nextInt();
+    }
+      //create a variable for total
+      int total = 0;
+      //create for loop for if the arrays are the same
+      for(int i = 0; i < list.length; i++){
+        //create a variable for the sum
+        int sum = list2[i] - list[i];
+        total = sum + total;
+      
+      
+    }
+    //if statement if the total is equal to 0 or not.
+      if(total != 0){
+      System.out.println("These arrays are not the same");
+    }else if (total == 0){
+      System.out.println("These arrays are the same");
+    }
+    
     
   }
 }
